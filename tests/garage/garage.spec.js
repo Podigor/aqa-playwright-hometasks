c92f57decef3b9d184739b9914dad8c0c239e498
Ihorodore <ihor@odore.com>
2024-04-15 09:53:14 +0300

    removed redundant variables declaration

--- tests/garage/garage.spec.js
+++ tests/garage/garage.spec.js
@@ -3,19 +3,15 @@ import {CARS} from '../../src/data/cars'
 
 test.describe('Garage', ()=> {
 
-    let addCarPopup
-    let editFirstCarPopup
-    let removeCarPopup
-
         test.afterEach(async ({userGaragePage})=> {
-            editFirstCarPopup = await userGaragePage.openFirstEditCarPopup()
+            const editFirstCarPopup = await userGaragePage.openFirstEditCarPopup()
             await editFirstCarPopup.waitLoaded()
-            removeCarPopup = await editFirstCarPopup.openRemoveCarPopup()
+            const removeCarPopup = await editFirstCarPopup.openRemoveCarPopup()
             await removeCarPopup.removeCar()
         })   
             test.only('should be able to add car', async ({userGaragePage})=>{
                 await expect(userGaragePage.addCarBtn).toBeVisible()
-                addCarPopup = await userGaragePage.openAddCarPopup()
+                const addCarPopup = await userGaragePage.openAddCarPopup()
                 await addCarPopup.waitLoaded()
                 await addCarPopup.addCar(CARS.BMW_X5_WITH_MILEAGE_1000km)
                 await expect(userGaragePage.carName.first()).toHaveText(`${CARS.BMW_X5_WITH_MILEAGE_1000km.brand} ${CARS.BMW_X5_WITH_MILEAGE_1000km.model}`)
