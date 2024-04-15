856f5a4bb956ca212f9a6e32069c0b41ae7e7945
Ihorodore <ihor@odore.com>
2024-04-16 00:57:41 +0300

    Home task for playwright network

--- src/fixtures/customFixtures.js
+++ src/fixtures/customFixtures.js
@@ -1,16 +1,27 @@
 import { test as base, expect as baseExpect} from '@playwright/test'
 import GaragePage from '../../src/pageObjects/GaragePage/GaragePage'
 import { USER_STORAGE_STATE_PATH } from '../constants'
+import HomePage from '../pageObjects/HomePage/HomePage'
 
 export const test = base.extend({
-    userGaragePage : async ({browser}, use) => {
+    homePage: async ({page}, use)=>{
+        const homePage = new HomePage(page)
+        await use(homePage)
+    },
+    page: async ({browser}, use)=>{
         const ctx = await browser.newContext({
             storageState: USER_STORAGE_STATE_PATH
         })
         const page = await ctx.newPage()
 
+        await use(page)
+
+        await ctx.close()
+    },
+    userGaragePage: async ({page}, use)=>{
         const userGaragePage = new GaragePage(page)
         await userGaragePage.navigate()
+
         await use(userGaragePage)
     }
 })
